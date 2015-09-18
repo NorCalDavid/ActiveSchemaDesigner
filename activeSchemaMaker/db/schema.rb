d79942fe629b0b459911c47f28af99b1178191d0
brenda_nguyen <email": null>
2015-09-18 14:37:11 -0700

    Fix another merge conflict between local branches

--- activeSchemaMaker/db/schema.rb
+++ activeSchemaMaker/db/schema.rb
@@ -11,11 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(version: 20150918204257) do
-=======
-ActiveRecord::Schema.define(version: 20150918193901) do
->>>>>>> bn-rails-database
+  bn-rails-database
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

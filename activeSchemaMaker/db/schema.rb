c6650747bf30a53416179301020f60de15d4814d
zakairobbins <zakairobbins@gmail.com>
2015-09-18 20:00:51 -0700

    add jqueryui, add layout css and nav bar

--- activeSchemaMaker/db/schema.rb
+++ activeSchemaMaker/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150918204257) do
-  bn-rails-database
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

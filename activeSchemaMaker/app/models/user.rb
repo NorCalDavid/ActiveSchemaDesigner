d3b63234c99f43a146c475011e5d66fe7564ee05
NorCalDavid <mail@daviduli.com>
2015-09-20 14:40:13 -0700

    Adjusted Filds Form

--- activeSchemaMaker/app/models/user.rb
+++ activeSchemaMaker/app/models/user.rb
@@ -4,10 +4,13 @@ class User < ActiveRecord::Base
   has_many :projects
 
   validates :first_name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true
+  validates :last_name, presence: true, length: { maximum: 50 }
+
+  validates :email, {
+                      :uniqueness => {:case_sensitive => false},
+                      :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+                    }
+
+  validates :password, :presence => true, :length => {minimum: 6}, :confirmation => true
 
 end

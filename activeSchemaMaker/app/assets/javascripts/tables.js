22411de9e7db97801efd94f04b04d43880ce506c
NorCalDavid <mail@daviduli.com>
2015-09-19 19:32:12 -0700

    Add Nested Attribute Form

--- activeSchemaMaker/app/assets/javascripts/tables.js
+++ activeSchemaMaker/app/assets/javascripts/tables.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on('click', 'form .add_fields', function(e) {
+  time = new Date().getTime();
+  regex = new RegExp($(this).data('id'), 'g');
+  $(this).before($(this).data('fields').replace(regex, time));
+  e.preventDefault();
+});
---
+++ activeSchemaMaker/app/helpers/form_helper.rb
@@ -0,0 +1,16 @@
+module FormHelper
+  def setup_table(table)
+    table.fields ||= Field.new
+    table
+  end
+
+  def add_object_link(name, form, object, partial, where)
+    options = {:parent => true}.merge(options)
+    html = render(:partial => partial, :locals => { :form => form}, :object => object)
+    link_to_function name, %{
+      var new_object_id = new Date().getTime() ;
+      var html = jQuery(#{js html}.replace(/index_to_replace_with_js/g, new_object_id)).hide();
+      html.appendTo(jQuery("#{where}")).slideDown('slow');
+    }
+end
+end

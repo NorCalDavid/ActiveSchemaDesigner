25464cfe7dc69620d0c208cb2509afb9b94e5d66
Tom2277 <Tom2277@users.noreply.github.com>
2015-09-19 19:19:09 -0700

    import current developoment branch to work with jquery drag and connect features

--- activeSchemaMaker/app/controllers/users_controller.rb
+++ activeSchemaMaker/app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end

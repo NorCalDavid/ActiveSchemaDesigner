bb5dc794066d32071f4dffc18225563da4290e80
NorCalDavid <mail@daviduli.com>
2015-09-20 11:53:54 -0700

    Add Validations to Fields DB

--- activeSchemaMaker/db/migrate/20150919011510_create_fields.rb
+++ activeSchemaMaker/db/migrate/20150919011510_create_fields.rb
@@ -4,8 +4,19 @@ class CreateFields < ActiveRecord::Migration
       t.string :name
       t.string :data_type
       t.string :default_value
+      # Validations
       t.boolean :auto_increment
       t.boolean :allow_null
+      t.boolean :presence
+      t.boolean :uniqueness
+      t.boolean :acceptance
+      t.boolean :confirmation
+      t.boolean :length
+      t.integer :length_min
+      t.integer :length_max
+      t.integer :length_is
+      t.integer :length_within
+
       t.references :table, index: true, foreign_key: true
 
       t.timestamps null: false

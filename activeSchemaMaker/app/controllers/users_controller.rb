89ad735e72f854b62dccf2f55eb1ae8b9b6adcbe
brenda_nguyen <email": null>
2015-09-20 18:08:23 -0700

    add styling for user#show view

--- activeSchemaMaker/app/controllers/users_controller.rb
+++ activeSchemaMaker/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -18,6 +20,7 @@ class UsersController < ApplicationController
   end
 
   def show
+
   end
 
   def edit
@@ -28,6 +31,12 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)

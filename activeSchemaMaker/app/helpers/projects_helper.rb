2a798282a54080a42fbf9158fe2f6a70aef717fc
NorCalDavid <mail@daviduli.com>
2015-09-22 10:06:31 -0700

    Fixed Bugs with Relationship Helpers and Main Layout

--- activeSchemaMaker/app/helpers/projects_helper.rb
+++ activeSchemaMaker/app/helpers/projects_helper.rb
@@ -1,11 +1,16 @@
 module ProjectsHelper
 
-def get_table_names(tables)
-  table_names = tables.map {|table| table.name}
-end
+  def get_table_names(tables)
+    table_names = tables.map {|table| table.name} unless tables.nil? || tables == ["No Tables"]
+  end
 
   def select_menu_table_names(table_names)
-    option_values = (1..table_names.length).to_a
-    table_names.zip(option_values)
+    if table_names.nil?
+      return ["No Tables"]
+    else
+      option_values = (1..table_names.length).to_a
+      table_names.zip(option_values)
     end
+  end
+
 end

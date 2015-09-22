f0d1fbcae088763d6318ccbaa14a38cbb1fb0424
NorCalDavid <mail@daviduli.com>
2015-09-21 17:07:45 -0700

    Add JQuery UI

--- activeSchemaMaker/app/assets/javascripts/tables.js
+++ activeSchemaMaker/app/assets/javascripts/tables.js
@@ -6,17 +6,17 @@ $(document).ready(function(){
 
   $(".draggable").draggable();
 
-  $(document).on('click', 'form .add_fields', function(e) {
-    time = new Date().getTime();
-    regex = new RegExp($(this).data('id'), 'g');
-    $(this).before($(this).data('fields').replace(regex, time));
-    e.preventDefault();
-  });
+  // $(document).on('click', 'form .add_fields', function(e) {
+  //   time = new Date().getTime();
+  //   regex = new RegExp($(this).data('id'), 'g');
+  //   $(this).before($(this).data('fields').replace(regex, time));
+  //   e.preventDefault();
+  // });
 
-  $(document).on('click', 'form .btn-add-comments', function(e) {
-    e.preventDefault();
-    $('form .comments-field').toggle();
-  });
+  // $(document).on('click', 'form .btn-add-comments', function(e) {
+  //   e.preventDefault();
+  //   $('form .comments-field').toggle();
+  // });
 
   $(".sortable").sortable();
 

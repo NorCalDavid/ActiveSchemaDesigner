928271f469e2d3033936afff2b3bfd3cbf0d7cd5
NorCalDavid <mail@daviduli.com>
2015-09-23 16:02:54 -0700

    Update DB Structure

--- activeSchemaMaker/db/migrate/20150923214410_add_position_to_table.rb
+++
@@ -1,6 +0,0 @@
-class AddPositionToTable < ActiveRecord::Migration
-  def change
-    add_column :tables, :position_y, :integer, default: 0
-    add_column :tables, :position_x, :integer, default: 0
-  end
-end

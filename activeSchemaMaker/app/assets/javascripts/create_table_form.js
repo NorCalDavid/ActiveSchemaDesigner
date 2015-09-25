c3a8ca3a6f4b338a5999ad87a8633ebabe7c5502
NorCalDavid <mail@daviduli.com>
2015-09-24 18:43:23 -0700

    Resolved Issues and Returned to Functioning State

--- activeSchemaMaker/app/assets/javascripts/create_table_form.js
+++ activeSchemaMaker/app/assets/javascripts/create_table_form.js
@@ -4,48 +4,8 @@ $(document).ready(function(){
     var time = new Date().getTime();
     var regex = new RegExp($(this).data('id'), 'g');
     $(this).before($(this).data('fields').replace(regex, time));
-    e.preventDefault();
-    // $('div.validation-options').hide()
+    event.preventDefault();
   });
 
-  $(document).on('click', '#create-table-form .btn-add-comments', function(e) {
-    e.preventDefault();
-    $('#create-table-form .comments-field').toggle()
-  });
-
-  $(document).on('click', 'a.btn-validations', function(e) {
-    e.preventDefault();
-    $(e.target).closest('h3').siblings('div').toggle();
-  });
-
-  $(function() {
-    $( "#accordion" ).accordion({
-      heightStyle: "content",
-      collapsible: true
-    });
-  });
-
-  $(function() {
-    $( "#tabs" ).tabs();
-  });
-
-  // main toolbar form submits new table
-  $('#create-table-form')
-    .on('ajax:success', function(event, response, xhr) {
-      $(".canvas").append(response);
-      $(".draggable").draggable();
-      // $('#create-table-form').reset();
-    })
-    .on("ajax:error", function(event){
-      console.error('failed to create table', arguments);
-    });
-
-  function resetCreateTableForm() {
-    $('#create-table-form').reset();
-  };
-
-  $(document).on('click', '#create-table-form .btn-ResetCTForm', function(event) {
-    $(event.target).preventDefault();
-    resetCreateTableForm();
-  });
 });
+
